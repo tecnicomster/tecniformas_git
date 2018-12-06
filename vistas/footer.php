3e98866001bb34e6d251d73bdb818e0665ece2ae
sebastian1235 <juseloco@gmail.com>
2018-12-06 17:29:29 -0500

    add
    cambio de version 1.0.1

--- vistas/footer.php
+++ vistas/footer.php
@@ -1,6 +1,6 @@
     <footer class="main-footer">
         <div class="pull-right hidden-xs">
-          <b>Version</b> 1.0.0
+          <b>Version</b> 1.0.1
         </div>
         <strong>Copyright &copy; 2013-2017 <a href="www.ticsia.com">Ticsia</a>.</strong> All rights reserved.
     </footer>    
